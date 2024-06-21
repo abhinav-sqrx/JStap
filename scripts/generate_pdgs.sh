653ce4e1fcae2b3f732dca4a66a2d6f257819420
Abhinav <abhinav@sqrx.com>
2024-06-21 17:00:04 +0530

    on demand intermediete step working

--- scripts/generate_pdgs.sh
+++ scripts/generate_pdgs.sh
@@ -5,9 +5,10 @@
 
 
 samples_path='../samples/Bad-validate'
+level=${1:-"pdg"}
 
 echo "Beginning PDGs generation from $samples_path"
 
-python3 -c "import sys; sys.path.insert(0, '../pdg_generation'); from pdgs_generation import *; store_pdg_folder('$samples_path')"
+python3 -c "import sys; sys.path.insert(0, '../pdg_generation'); from pdgs_generation import *; store_pdg_folder('$samples_path', '$level')"
 
 echo "Finished PDGs generation"
